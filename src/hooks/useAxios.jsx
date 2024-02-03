f7ef17f728ebb61247836ef726661c656f898780
Mohin Rana <ebrahimit49@gmail.com>
2024-02-03 22:22:46 +0600

    Server live url added

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://rent-houses-server-nextjs.vercel.app/api/v1',
     withCredentials: true
 })
 

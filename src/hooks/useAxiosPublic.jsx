9c2f1cf42272a52d7f73a4bcde95ec78df7d3b66
Mohin Rana <ebrahimit49@gmail.com>
2024-02-03 22:31:00 +0600

    link update

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instancePublic = axios.create({
-    baseURL: 'http://localhost:5000/api/v1',
+    baseURL: 'https://rent-houses-server-nextjs.vercel.app/api/v1',
 })
 const useAxiosPublic = () => {
     return instancePublic
